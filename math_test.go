29e049a559b6b36250ae34b1c2276ed7512a3208
Gabriel Costa <gubsocosta@gmail.com>
2023-05-09 15:47:19 -0300

    test: :bug: adjust test file

--- math_test.go
+++ math_test.go
@@ -7,6 +7,6 @@ func TestSum(t *testing.T) {
 	total := sum(15, 15)
 
 	if total != 30 {
-		t.ErrorF("Invalid sum result: Expected -> %d Received -> %d", total, 30)
+		t.Errorf("Invalid sum result: Expected -> %d Received -> %d", total, 30)
 	}
 }
